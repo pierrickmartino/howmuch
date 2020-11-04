f637be88e3b34900a51e5729663d492ec9a2034c
pierrick.martino <pierrick.martino@gmail.com>
2020-11-04 21:13:59 +0100

    test sidebar with fluid layout content

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:collapsible_sidebar/collapsible_sidebar.dart'; // https://pub.dev/packages/collapsible_sidebar
+import 'dart:math' as math show pi;
+//import 'package:responsive_framework/responsive_framework.dart'; // https://pub.dev/packages/responsive_framework
+import 'package:fluid_layout/fluid_layout.dart';
+
+import 'widgets/custom_card.dart'; // https://pub.dev/packages/fluid_layout
 
 void main() {
   runApp(MyApp());
@@ -9,105 +15,276 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: SidebarPage(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SidebarPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SidebarPageState createState() => _SidebarPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SidebarPageState extends State<SidebarPage> {
+  List<CollapsibleItem> _items;
+  String _headline;
+  NetworkImage _avatarImg =
+      NetworkImage('https://www.w3schools.com/howto/img_avatar.png');
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _items = _generateItems;
+    _headline = _items.firstWhere((item) => item.isSelected).text;
+  }
+
+  List<CollapsibleItem> get _generateItems {
+    return [
+      CollapsibleItem(
+        text: 'Dashboard',
+        icon: Icons.assessment,
+        onPressed: () => setState(() => _headline = 'DashBoard'),
+        isSelected: true,
+      ),
+      CollapsibleItem(
+        text: 'Errors',
+        icon: Icons.cancel,
+        onPressed: () => setState(() => _headline = 'Errors'),
+      ),
+      CollapsibleItem(
+        text: 'Search',
+        icon: Icons.search,
+        onPressed: () => setState(() => _headline = 'Search'),
+      ),
+      CollapsibleItem(
+        text: 'Notifications',
+        icon: Icons.notifications,
+        onPressed: () => setState(() => _headline = 'Notifications'),
+      ),
+      CollapsibleItem(
+        text: 'Settings',
+        icon: Icons.settings,
+        onPressed: () => setState(() => _headline = 'Settings'),
+      ),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    var size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: CollapsibleSidebar(
+        items: _items,
+        avatarImg: _avatarImg,
+        title: 'John Smith',
+        body: _body(size, context),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _body(Size size, BuildContext context) {
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.blueGrey[50],
+        child: FluidLayout(
+            child: Builder(
+                builder: (context) => CustomScrollView(
+                      slivers: <Widget>[
+                        SliverToBoxAdapter(
+                            child: SizedBox(
+                                height: context.fluid(60, xs: 12, s: 12))),
+                        SliverFluidGrid(
+                          children: [
+                            if (context.breakpoint.isSmallerThanM)
+                              FluidCell.fit(
+                                  size: context.fluid(12),
+                                  child: CustomCard(
+                                      child: Container(
+                                    height: 100,
+                                    child: Center(
+                                        child: Text(
+                                            'Only visible in small screens')),
+                                  ))),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(12),
+                                heightSize: context.fluid(4),
+                                child: CustomCard(
+                                  color: Colors.green,
+                                  child: Center(child: Text('Header')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 3, s: 6, xs: 6),
+                                heightSize: context.fluid(3, m: 3, s: 6, xs: 6),
+                                child: CustomCard(
+                                  child: Center(child: Text('A')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 3, s: 6, xs: 6),
+                                heightSize: context.fluid(3, m: 3, s: 4, xs: 4),
+                                child: CustomCard(
+                                  child: Center(child: Text('B')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 3, s: 6, xs: 6),
+                                heightSize: context.fluid(3, m: 3, s: 6, xs: 6),
+                                child: CustomCard(
+                                  child: Center(child: Text('C')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 3, s: 6, xs: 6),
+                                heightSize: context.fluid(3, m: 3, s: 4, xs: 4),
+                                child: CustomCard(
+                                  child: Center(child: Text('D')),
+                                )),
+                            FluidCell.withFixedHeight(
+                                size: context.fluid(12),
+                                height: 1,
+                                child: Container(
+                                  color: Colors.black12,
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 6, s: 12, xs: 12),
+                                heightSize: 3,
+                                child: CustomCard(
+                                  color: Colors.green,
+                                  child: Center(child: Text('1')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 6, s: 6, xs: 6),
+                                heightSize: context.fluid(1.45, s: 2, xs: 2),
+                                child: CustomCard(
+                                  child: Center(child: Text('2')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 6, s: 6, xs: 6),
+                                heightSize: context.fluid(1.45, s: 2, xs: 2),
+                                child: CustomCard(
+                                  child: Center(child: Text('3')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 6, s: 6, xs: 6),
+                                heightSize: context.fluid(1.45, s: 2, xs: 2),
+                                child: CustomCard(
+                                  child: Center(child: Text('4')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 6, s: 6, xs: 6),
+                                heightSize: context.fluid(1.45, s: 2, xs: 2),
+                                child: CustomCard(
+                                  child: Center(child: Text('5')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 6, s: 6, xs: 6),
+                                heightSize: context.fluid(1.45, s: 2, xs: 2),
+                                child: CustomCard(
+                                  child: Center(child: Text('6')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(3, m: 6, s: 6, xs: 6),
+                                heightSize: context.fluid(1.45, s: 2, xs: 2),
+                                child: CustomCard(
+                                  child: Center(child: Text('7')),
+                                )),
+                            FluidCell.withFixedHeight(
+                                size: 12,
+                                height: 150,
+                                child: CustomCard(
+                                  color: Colors.green,
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(2, l: 2, m: 4, s: 3, xs: 4),
+                                heightSize:
+                                    context.fluid(2, l: 2, m: 3, s: 3, xs: 4),
+                                child: CustomCard(
+                                  child: Center(child: Text('I')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(2, l: 2, m: 4, s: 3, xs: 4),
+                                heightSize:
+                                    context.fluid(2, l: 2, m: 3, s: 3, xs: 4),
+                                child: CustomCard(
+                                  child: Center(child: Text('I')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(2, l: 2, m: 4, s: 3, xs: 4),
+                                heightSize:
+                                    context.fluid(2, l: 2, m: 3, s: 3, xs: 4),
+                                child: CustomCard(
+                                  child: Center(child: Text('I')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(2, l: 2, m: 4, s: 3, xs: 4),
+                                heightSize:
+                                    context.fluid(2, l: 2, m: 3, s: 3, xs: 4),
+                                child: CustomCard(
+                                  child: Center(child: Text('I')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(2, l: 2, m: 4, s: 3, xs: 4),
+                                heightSize:
+                                    context.fluid(2, l: 2, m: 3, s: 3, xs: 4),
+                                child: CustomCard(
+                                  child: Center(child: Text('I')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(2, l: 2, m: 4, s: 3, xs: 4),
+                                heightSize:
+                                    context.fluid(2, l: 2, m: 3, s: 3, xs: 4),
+                                child: CustomCard(
+                                  child: Center(child: Text('I')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(4, s: 12, xs: 12),
+                                heightSize: context.fluid(4, s: 12, xs: 12),
+                                child: CustomCard(
+                                  child: Center(child: Text('A')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(4, s: 12, xs: 12),
+                                heightSize: context.fluid(4, s: 12, xs: 12),
+                                child: CustomCard(
+                                  child: Center(child: Text('B')),
+                                )),
+                            FluidCell.withFluidHeight(
+                                size: context.fluid(4, s: 12, xs: 12),
+                                heightSize: context.fluid(4, s: 12, xs: 12),
+                                child: CustomCard(
+                                  child: Center(child: Text('C')),
+                                )),
+                          ],
+                        ),
+                        SliverToBoxAdapter(
+                          child: Container(
+                            margin: EdgeInsets.symmetric(vertical: 12),
+                            padding: EdgeInsets.symmetric(vertical: 12),
+                            color: Colors.green,
+                            height: 200,
+                            child: Center(child: Text('Expanded')),
+                          ),
+                        ),
+                        SliverToBoxAdapter(child: SizedBox(height: 40)),
+                        context.breakpoint.isLargerThanM
+                            ? SliverFluid(
+                                sliver: SliverToBoxAdapter(
+                                    child: CustomCard(
+                                        child: Container(
+                                  height: 300,
+                                  child: Center(
+                                      child:
+                                          Text('Only visible in large screen')),
+                                ))),
+                              )
+                            : SliverToBoxAdapter(child: Container()),
+                        SliverToBoxAdapter(child: SizedBox(height: 40)),
+                      ],
+                    ))));
   }
 }
