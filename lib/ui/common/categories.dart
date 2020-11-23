8152fc722694c3ff043c2b4e362df51ffd3ea48a
pierrick.martino <pierrick.martino@gmail.com>
2020-11-23 08:37:04 +0100

    grah extraction in a dedicated widget

--- lib/ui/common/categories.dart
+++ lib/ui/common/categories.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluid_layout/fluid_layout.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../chart/example_bar_chart.dart';
 import 'package:line_awesome_icons/line_awesome_icons.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:undo/undo.dart';
 
 import 'add_category_dialog.dart';
@@ -16,26 +16,11 @@ class Categories extends StatefulWidget {
   _CategoriesState createState() => _CategoriesState();
 }
 
-class _SalesData {
-  _SalesData(this.year, this.sales);
-
-  final double year;
-  final double sales;
-}
-
 class _CategoriesState extends State<Categories> {
   HowMuchAppBloc get bloc => BlocProvider.of<HowMuchAppBloc>(context);
 
   @override
   Widget build(BuildContext context) {
-    final List<_SalesData> chartData = [
-      _SalesData(2010, 35),
-      _SalesData(2011, 28),
-      _SalesData(2012, 34),
-      _SalesData(2013, 32),
-      _SalesData(2014, 40)
-    ];
-
     return BlocBuilder<HowMuchAppBloc, ChangeStack>(
         builder: (context, cs) => Scaffold(
               floatingActionButton: FloatingActionButton(
@@ -86,41 +71,8 @@ class _CategoriesState extends State<Categories> {
                                         FluidLayout.of(context).fluidPadding),
                                 shrinkWrap: true,
                                 scrollDirection: Axis.horizontal,
-                                itemBuilder: (context, index) => CustomCard(
-                                    //color: Colors.green,
-                                    child: SfCartesianChart(
-                                        primaryXAxis: CategoryAxis(),
-                                        // Chart title
-                                        title: ChartTitle(
-                                            textStyle: Theme.of(context)
-                                                .textTheme
-                                                .subtitle2,
-                                            alignment: ChartAlignment.near,
-                                            text: 'Half yearly sales analysis'),
-
-                                        // Enable legend
-                                        legend: Legend(isVisible: false),
-                                        // Enable tooltip
-                                        tooltipBehavior:
-                                            TooltipBehavior(enable: true),
-                                        series: <ChartSeries>[
-                                      LineSeries<_SalesData, double>(
-                                          dataSource: chartData,
-                                          xValueMapper: (_SalesData sales, _) =>
-                                              sales.year,
-                                          yValueMapper: (_SalesData sales, _) =>
-                                              sales.sales,
-                                          // Enable data label
-                                          dataLabelSettings: DataLabelSettings(
-                                              isVisible: true))
-                                    ])
-                                    // Container(
-                                    //     height: 180,
-                                    //     width: 200,
-                                    //     child: Center(
-                                    //       child: Text('Item'),
-                                    //     ))
-                                    ),
+                                itemBuilder: (context, index) =>
+                                    ExempleBarChart(),
                                 separatorBuilder: (_, __) => SizedBox(
                                     width: FluidLayout.of(context)
                                         .horizontalPadding),
