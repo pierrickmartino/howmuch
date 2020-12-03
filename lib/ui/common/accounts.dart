68e9211e8893b4cf8b47dc3568c8f2644c498ca1
pierrickmartino <pierrick.martino@gmail.com>
2020-12-03 15:30:36 +0100

    Theme Fixes #35

--- lib/ui/common/accounts.dart
+++ lib/ui/common/accounts.dart
@@ -1,4 +1,8 @@
+import 'package:fluid_layout/fluid_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:line_awesome_icons/line_awesome_icons.dart';
+
+import '../../constant/const.dart';
 
 class Accounts extends StatefulWidget {
   @override
@@ -8,8 +12,55 @@ class Accounts extends StatefulWidget {
 class _AccountsState extends State<Accounts> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Accounts"),
-    );
+    GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+    return Scaffold(
+        key: _scaffoldKey,
+        body: Container(
+            color: Color(backgroundColor),
+            child: FluidLayout(
+                child: Builder(
+                    builder: (context) => SingleChildScrollView(
+                            child: Column(children: <Widget>[
+                          SizedBox(height: 6),
+                          Fluid(
+                              child: Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: Color(menuBackgroundColor),
+                              borderRadius: BorderRadius.all(Radius.circular(
+                                      5.0) //         <--- border radius here
+                                  ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color(menuBackgroundColor),
+                                  blurRadius: 10,
+                                  spreadRadius: 0.01,
+                                  offset: Offset(3, 3),
+                                ),
+                              ],
+                            ),
+                            height: 50,
+                            width: double.infinity,
+                            child: Row(
+                              children: [
+                                Text(
+                                  'Accounts',
+                                  style: TextStyle(color: Color(menuTextColor)),
+                                ),
+                                Spacer(),
+                                IconButton(
+                                  onPressed: () {},
+                                  tooltip: 'New account',
+                                  icon: Icon(
+                                    LineAwesomeIcons.plus,
+                                    size: 16,
+                                    color: Color(menuTextColor),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ))
+                        ]))))));
   }
 }
