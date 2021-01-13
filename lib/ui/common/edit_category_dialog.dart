5ef6ec7c843490abcc720c5d0f9c5cb5abef5b65
pierrickmartino <pierrick.martino@gmail.com>
2021-01-13 17:03:02 +0100

    category - fix edition bug Fixes #80

--- lib/ui/common/edit_category_dialog.dart
+++ lib/ui/common/edit_category_dialog.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_iconpicker/flutter_iconpicker.dart';
 import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
-import 'package:howmuch/constant/const.dart';
-import 'package:howmuch/src/model/category.dart';
-//import 'package:intl/intl.dart';
+import 'package:howmuch/src/bloc/category.dart';
+import 'package:howmuch/src/database/database.dart';
 import 'package:line_awesome_icons/line_awesome_icons.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
-// UNUSED IN FAKE DATA MODE
-// import '../../src/utils/category_utils_graphql.dart';
-
-// final _dateFormat = DateFormat.yMMMd();
-
 class CategoryEditDialog extends StatefulWidget {
-  final entry;
+  final Category entry;
   //final Function() notifyParent;
   final BuildContext contextParent;
 
@@ -27,94 +22,53 @@ class CategoryEditDialog extends StatefulWidget {
 class _CategoryEditDialogState extends State<CategoryEditDialog> {
   // standard field
   final TextEditingController textController = TextEditingController();
-  //DateTime _creationDate;
   // color picker
-  ColorSwatch _categoryColor;
+  ColorSwatch _categoryColor, _tempCategoryColor;
   MaterialColor _categoryMaterialColor;
-  ColorSwatch _tempCategoryColor;
   // icon picker
-  Icon _categoryIcon;
-  Icon _icon;
-  bool isAdaptive = true;
-  bool showTooltips = false;
-  bool showSearch = true;
+  Icon _categoryIcon, _icon;
+  bool isAdaptive = true, showTooltips = false, showSearch = true;
   int performance = 0;
-
-  String storedCategoryId,
-      storedCategoryName,
-      storedCategoryIconFamily,
-      storedCategoryIconPackage;
-  int storedCategoryColor,
-      storedCategoryPerformance,
-      storedCategoryCounter,
-      storedCategoryIcon;
 
   @override
   void initState() {
-    if (dataMode == 'fake') {
-      Category fakeCategory = widget.entry;
+    textController.text = widget.entry.description;
+    performance =
+        widget.entry.performance ?? 0; // 0 if it's null; otherwise, the integer
 
-      storedCategoryId = fakeCategory.id;
-      storedCategoryName = fakeCategory.name;
-      storedCategoryColor = fakeCategory.color;
-      storedCategoryPerformance = fakeCategory.performance;
-      storedCategoryCounter = fakeCategory.counter;
-      storedCategoryIcon = fakeCategory.icon;
-      storedCategoryIconFamily = fakeCategory.iconfamily;
-      storedCategoryIconPackage = fakeCategory.iconpackage;
-    } else {
-      storedCategoryId = widget.entry["node"]['id'];
-      storedCategoryName = widget.entry["node"]['name'];
-      storedCategoryColor = widget.entry["node"]['color'];
-      storedCategoryPerformance = widget.entry["node"]['performance'];
-      storedCategoryCounter = widget.entry["node"]['counter'];
-      storedCategoryIcon = widget.entry["node"]['icon'];
-      storedCategoryIconFamily = widget.entry["node"]['iconfamily'];
-      storedCategoryIconPackage = widget.entry["node"]['iconpackage'];
-    }
-
-    textController.text = storedCategoryName;
-    performance = storedCategoryPerformance ??
-        0; // 0 if it's null; otherwise, the integer
-    //_creationDate = widget.entry.creationDate;
-
-    if (storedCategoryColor == null) {
+    if (widget.entry.color == null) {
       _categoryColor = Colors.grey;
       _categoryMaterialColor = Colors.grey;
     } else {
-      Map<int, Color> color = {
-        100: Color(storedCategoryColor).withOpacity(1),
-      };
-      _categoryMaterialColor = MaterialColor(storedCategoryColor, color);
+      Map<int, Color> color = {100: Color(widget.entry.color).withOpacity(1)};
+      _categoryMaterialColor = MaterialColor(widget.entry.color, color);
       _categoryColor = _categoryMaterialColor;
     }
 
-    if (storedCategoryIcon == null) {
+    if (widget.entry.icon == null) {
       _categoryIcon = Icon(LineAwesomeIcons.info_circle);
     } else {
-      _categoryIcon = Icon(IconData(storedCategoryIcon,
-          fontFamily: storedCategoryIconFamily,
-          fontPackage: storedCategoryIconPackage));
+      _categoryIcon = Icon(IconData(widget.entry.icon,
+          fontFamily: widget.entry.iconFamily,
+          fontPackage: widget.entry.iconPackage));
     }
 
     super.initState();
   }
 
-  //@override
-  // void dispose() {
-  //   textController.dispose();
-  //   super.dispose();
-  // }
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
 
   void _openCategoryIcon() async {
-    IconData icon = await FlutterIconPicker.showIconPicker(
-      context,
-      adaptiveDialog: isAdaptive,
-      showTooltips: showTooltips,
-      showSearchBar: showSearch,
-      barrierDismissible: false,
-      iconPackMode: IconPack.lineAwesomeIcons,
-    );
+    IconData icon = await FlutterIconPicker.showIconPicker(context,
+        adaptiveDialog: isAdaptive,
+        showTooltips: showTooltips,
+        showSearchBar: showSearch,
+        barrierDismissible: false,
+        iconPackMode: IconPack.lineAwesomeIcons);
 
     if (icon != null) {
       _icon = Icon(icon);
@@ -124,151 +78,100 @@ class _CategoryEditDialogState extends State<CategoryEditDialog> {
 
   void _openCategoryColorPicker() async {
     _openDialog(
-      "Main Color picker",
-      MaterialColorPicker(
-        colors: fullMaterialColors,
-        selectedColor: _categoryColor,
-        allowShades: false,
-        circleSize: 35.0,
-        onMainColorChange: (color) =>
-            setState(() => _tempCategoryColor = color),
-      ),
-    );
+        "Main Color picker",
+        MaterialColorPicker(
+            colors: fullMaterialColors,
+            selectedColor: _categoryColor,
+            allowShades: false,
+            circleSize: 35.0,
+            onMainColorChange: (color) =>
+                setState(() => _tempCategoryColor = color)));
   }
 
   void _openDialog(String title, Widget content) {
     showDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (_) {
-        return AlertDialog(
-          title: Text(title),
-          content: content,
-          actions: [
+        barrierDismissible: false,
+        context: context,
+        builder: (_) {
+          return AlertDialog(title: Text(title), content: content, actions: [
             FlatButton(
-              child: Text('Cancel'),
-              onPressed: Navigator.of(context).pop,
-            ),
+                child: Text('Cancel'), onPressed: Navigator.of(context).pop),
             FlatButton(
-              child: Text('Save'),
-              onPressed: () {
-                Navigator.of(context).pop();
-                setState(() => _categoryColor = _tempCategoryColor);
-              },
-            ),
-          ],
-        );
-      },
-    );
+                child: Text('Save'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  setState(() => _categoryColor = _tempCategoryColor);
+                })
+          ]);
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    //var formattedDate = 'No creation date set';
-    // if (_creationDate != null) {
-    //   formattedDate = _dateFormat.format(_creationDate);
-    // }
-
     return AlertDialog(
       title: const Text('Edit category'),
       actionsPadding: const EdgeInsets.symmetric(horizontal: 8.0),
       contentPadding: const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 24.0),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
+      content: Column(mainAxisSize: MainAxisSize.min, children: [
+        TextField(
             controller: textController,
             decoration: InputDecoration(
-              hintText: 'What needs to be done?',
-              helperText: 'Content of entry',
-            ),
-          ),
-          SizedBox(height: 12),
-          Row(
-            children: [
-              Text('Color'),
-              Spacer(),
-              CircleColor(color: _categoryColor, circleSize: 25),
-              IconButton(
-                icon: const Icon(LineAwesomeIcons.edit),
-                onPressed: _openCategoryColorPicker,
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text('Icon'),
-              Spacer(),
-              _categoryIcon,
-              IconButton(
-                icon: const Icon(LineAwesomeIcons.edit),
-                onPressed: _openCategoryIcon,
-              )
-            ],
-          ),
-          SizedBox(height: 12),
-          Row(
-            children: [
-              Text('Perf.'),
-              Spacer(),
-              ToggleSwitch(
-                initialLabelIndex: performance,
-                cornerRadius: 12.0,
-                minWidth: 65,
-                labels: ['incl.', 'or not'],
-                onToggle: (index) {
-                  performance = index;
-//                  print('switched to: $index');
-                },
-              ),
-            ],
-          ),
-        ],
-      ),
+                hintText: 'What needs to be done?',
+                helperText: 'Content of entry')),
+        SizedBox(height: 12),
+        Row(children: [
+          Text('Color'),
+          Spacer(),
+          CircleColor(color: _categoryColor, circleSize: 25),
+          IconButton(
+              icon: const Icon(LineAwesomeIcons.edit),
+              onPressed: _openCategoryColorPicker)
+        ]),
+        Row(children: [
+          Text('Icon'),
+          Spacer(),
+          _categoryIcon,
+          IconButton(
+              icon: const Icon(LineAwesomeIcons.edit),
+              onPressed: _openCategoryIcon)
+        ]),
+        SizedBox(height: 12),
+        Row(children: [
+          Text('Perf.'),
+          Spacer(),
+          ToggleSwitch(
+              initialLabelIndex: performance,
+              cornerRadius: 12.0,
+              minWidth: 65,
+              labels: ['incl.', 'or not'],
+              onToggle: (index) {
+                performance = index;
+              })
+        ])
+      ]),
       actions: [
         FlatButton(
-          child: const Text('Cancel'),
-          textColor: Colors.black,
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+            child: const Text('Cancel'),
+            textColor: Colors.black,
+            onPressed: () {
+              Navigator.pop(context);
+            }),
         FlatButton(
           child: const Text('Save'),
           onPressed: () {
             final updatedContent = textController.text;
-            // final entry = widget.entry["node"];
-            // entry['name'] = updatedContent.isNotEmpty ? updatedContent : null;
-            // entry['color'] = _categoryColor.value;
-            // entry['icon'] = _categoryIcon.icon.codePoint;
-            // entry['iconfamily'] = _categoryIcon.icon.fontFamily;
-            // entry['iconpackage'] = _categoryIcon.icon.fontPackage;
-            // entry['performance'] = performance;
 
-            //CategoryUtilsGraphQL utils;
+            final entry = widget.entry.copyWith(
+                description: updatedContent.isNotEmpty ? updatedContent : null,
+                color: _categoryColor.value,
+                icon: _categoryIcon.icon.codePoint,
+                iconFamily: _categoryIcon.icon.fontFamily,
+                iconPackage: _categoryIcon.icon.fontPackage,
+                performance: performance);
 
-            // if (entry["node"]['name'] &&
-            //     entry["node"]['color'].toString().isNotEmpty &&
-            //     entry.icon.toString().isNotEmpty) {
-            // utils = CategoryUtilsGraphQL(
-            //     id: entry['id'],
-            //     name: entry['name'],
-            //     color: entry['color'],
-            //     icon: entry['icon'],
-            //     iconfamily: entry['iconfamily'],
-            //     iconpackage: entry['iconpackage'],
-            //     performance: entry['performance'],
-            //     counter: 0);
-            // utils.updateData().whenComplete(
-            //     () => ScaffoldMessenger.of(widget.contextParent).showSnackBar(
-            //           SnackBar(
-            //             content: const Text('Category updated'),
-            //           ),
-            //         ));
-
-            print('edit category: ' + updatedContent);
             textController.text = "";
 
+            BlocProvider.of<HowMuchAppBloc>(context).updateCategory(entry);
             Navigator.pop(context);
           },
         ),
