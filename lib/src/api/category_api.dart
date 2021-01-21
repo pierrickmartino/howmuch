853015cf4c5fc674575e15562f1fbbd70bbe0531
pierrickmartino <pierrick.martino@gmail.com>
2021-01-21 21:14:42 +0100

    minor update

--- lib/src/api/category_api.dart
+++ lib/src/api/category_api.dart
@@ -1,60 +1,60 @@
-//Read
-final String findCategoriesQuery = """
-query findCategories {
-  categories(order: name_ASC) {
-    count
-    edges {
-      node {
-        id
-        objectId
-        name
-        color
-        icon
-        iconfamily
-        iconpackage
-        performance
-        counter
-      }
-    }
-  }
-}
-""";
+// //Read
+// final String findCategoriesQuery = """
+// query findCategories {
+//   categories(order: name_ASC) {
+//     count
+//     edges {
+//       node {
+//         id
+//         objectId
+//         name
+//         color
+//         icon
+//         iconfamily
+//         iconpackage
+//         performance
+//         counter
+//       }
+//     }
+//   }
+// }
+// """;
 
-//Create
-final String createCategoryMutation = """
-mutation createCategory (\$name : String!){
-  createCategory(input: { fields: { name: \$name }} ) {
-    category {
-      name
-      id
-      createdAt
-    }
-  }
-}
-""";
+// //Create
+// final String createCategoryMutation = """
+// mutation createCategory (\$name : String!){
+//   createCategory(input: { fields: { name: \$name }} ) {
+//     category {
+//       name
+//       id
+//       createdAt
+//     }
+//   }
+// }
+// """;
 
-//Update
-final String updateCategoryMutation = """
-mutation UpdateCategory (\$id: ID!, \$input: UpdateCategoryFieldsInput ){
-  updateCategory(
-    input: {id:\$id, fields:\$input}
-  ) {
-    category {
-      name
-      updatedAt
-    }
-  }
-}
-""";
+// //Update
+// final String updateCategoryMutation = """
+// mutation UpdateCategory (\$id: ID!, \$input: UpdateCategoryFieldsInput ){
+//   updateCategory(
+//     input: {id:\$id, fields:\$input}
+//   ) {
+//     category {
+//       name
+//       updatedAt
+//     }
+//   }
+// }
+// """;
 
-//Delete
-final String deleteCategoryMutation = """
-mutation DeleteCategory (\$id: ID!) {
-  deleteCategory(input: { id: \$id}) {
-    category {
-      name
-      id
-    }
-  }
-}
-""";
+// //Delete
+// final String deleteCategoryMutation = """
+// mutation DeleteCategory (\$id: ID!) {
+//   deleteCategory(input: { id: \$id}) {
+//     category {
+//       name
+//       id
+//     }
+//   }
+// }
+// """;
